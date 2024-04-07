b0210083d4ffb484dcbd27930d6bfa3a1a19b4aa
YaEtoTui <sazhin2003.egor@gmail.com>
2024-04-07 21:02:11 +0500

    change Activity(Map) -> Fragment

--- app/src/main/java/balacods/pp/restorambaapp/fragment/YandexCardFragment.kt
+++ app/src/main/java/balacods/pp/restorambaapp/fragment/YandexCardFragment.kt
@@ -4,22 +4,78 @@ import android.os.Bundle
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import androidx.core.content.ContextCompat
 import androidx.fragment.app.Fragment
 import balacods.pp.restorambaapp.R
 import balacods.pp.restorambaapp.databinding.FragmentYandexCardBinding
-import com.yandex.mapkit.Animation
 import com.yandex.mapkit.MapKitFactory
+import com.yandex.mapkit.RequestPoint
+import com.yandex.mapkit.RequestPointType
+import com.yandex.mapkit.directions.DirectionsFactory
+import com.yandex.mapkit.directions.driving.DrivingOptions
+import com.yandex.mapkit.directions.driving.DrivingRoute
+import com.yandex.mapkit.directions.driving.DrivingRouter
+import com.yandex.mapkit.directions.driving.DrivingRouterType
+import com.yandex.mapkit.directions.driving.DrivingSession
+import com.yandex.mapkit.directions.driving.VehicleOptions
 import com.yandex.mapkit.geometry.Point
 import com.yandex.mapkit.map.CameraPosition
+import com.yandex.mapkit.map.IconStyle
+import com.yandex.mapkit.map.InputListener
+import com.yandex.mapkit.map.Map
+import com.yandex.mapkit.map.MapObjectCollection
+import com.yandex.mapkit.map.PolylineMapObject
 import com.yandex.mapkit.mapview.MapView
+import com.yandex.mapkitdemo.common.CommonColors
+import com.yandex.mapkitdemo.common.showToast
+import com.yandex.runtime.Error
 import com.yandex.runtime.image.ImageProvider
+import com.yandex.runtime.network.NetworkError
 
 class YandexCardFragment : Fragment() {
 
     private lateinit var binding: FragmentYandexCardBinding
 
     lateinit var mapView: MapView
-    lateinit var imageProvider: ImageProvider
+    private lateinit var map: Map
+
+    private lateinit var drivingRouter: DrivingRouter
+    private var drivingSession: DrivingSession? = null
+    private lateinit var placemarksCollection: MapObjectCollection
+    private lateinit var routesCollection: MapObjectCollection
+
+    private val inputListener = object : InputListener {
+        override fun onMapTap(map: Map, point: Point) = Unit
+
+        override fun onMapLongTap(map: Map, point: Point) {
+            routePoints = routePoints + point
+        }
+    }
+
+    private val drivingRouteListener = object : DrivingSession.DrivingRouteListener {
+        override fun onDrivingRoutes(drivingRoutes: MutableList<DrivingRoute>) {
+            routes = drivingRoutes
+        }
+
+        override fun onDrivingRoutesError(error: Error) {
+            when (error) {
+                is NetworkError -> context!!.showToast("Routes request error due network issues")
+                else -> context!!.showToast("Routes request unknown error")
+            }
+        }
+    }
+
+    private var routePoints = emptyList<Point>()
+        set(value) {
+            field = value
+            onRoutePointsUpdated()
+        }
+
+    private var routes = emptyList<DrivingRoute>()
+        set(value) {
+            field = value
+            onRoutesUpdated()
+        }
 
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
@@ -28,6 +84,21 @@ class YandexCardFragment : Fragment() {
         MapKitFactory.initialize(this.context)
         binding = FragmentYandexCardBinding.inflate(inflater, container, false)
         return binding.root
+    }
+
+    private lateinit var pointStart: Point
+    private lateinit var pointEnd: Point
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+
+        pointStart = Point(56.797469, 60.714430)
+
+        mapView = binding.imCarteGeo
+        map = mapView.mapWindow.map
+        map.addInputListener(inputListener)
+        showCardYandex()
+
     }
 
     override fun onStop() {
@@ -42,21 +113,94 @@ class YandexCardFragment : Fragment() {
         super.onStart()
     }
 
-    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
-        super.onViewCreated(view, savedInstanceState)
+    private fun showCardYandex() {
 
-        mapView = binding.imCarteGeo
-        imageProvider = ImageProvider.fromResource(this.context, R.drawable.icon_location_2)
-        showCardYandex()
+        placemarksCollection = map.mapObjects.addCollection()
+        routesCollection = map.mapObjects.addCollection()
 
+        drivingRouter = DirectionsFactory.getInstance().createDrivingRouter(DrivingRouterType.COMBINED)
+
+        binding.buttonClearRoute.setOnClickListener {
+            routePoints = emptyList()
+        }
+
+        val map = mapView.mapWindow.map
+        map.move(START_POSITION)
+
+        routePoints = DEFAULT_POINTS
     }
 
-    private fun showCardYandex() {
-        mapView.map.move(
-            CameraPosition(Point(56.8315336317274, 60.60252916883852), 16.0f, 0.0f, 0.0f),
-            Animation(Animation.Type.SMOOTH, 1f), null
-        )
+    private fun onRoutePointsUpdated() {
+        placemarksCollection.clear()
 
-        val placemark = mapView.map.mapObjects.addPlacemark(Point(56.8315336317274, 60.60252916883852), imageProvider)
+        if (routePoints.isEmpty()) {
+            drivingSession?.cancel()
+            routes = emptyList()
+            return
+        }
+
+        val imageProvider = ImageProvider.fromResource(requireContext(), R.drawable.bullet)
+        routePoints.forEach {
+            placemarksCollection.addPlacemark(
+                it,
+                imageProvider,
+                IconStyle().apply {
+                    scale = 0.5f
+                    zIndex = 20f
+                }
+            )
+        }
+
+        if (routePoints.size < 2) return
+
+        val requestPoints = buildList {
+            add(RequestPoint(routePoints.first(), RequestPointType.WAYPOINT, null, null))
+            addAll(
+                routePoints.subList(1, routePoints.size - 1)
+                    .map { RequestPoint(it, RequestPointType.VIAPOINT, null, null) })
+            add(RequestPoint(routePoints.last(), RequestPointType.WAYPOINT, null, null))
+        }
+
+        val drivingOptions = DrivingOptions()
+        val vehicleOptions = VehicleOptions()
+
+        drivingSession = drivingRouter.requestRoutes(
+            requestPoints,
+            drivingOptions,
+            vehicleOptions,
+            drivingRouteListener,
+        )
+    }
+
+    private fun onRoutesUpdated() {
+        routesCollection.clear()
+        if (routes.isEmpty()) return
+
+        routes.forEachIndexed { index, route ->
+            routesCollection.addPolyline(route.geometry).apply {
+                if (index == 0) {
+                    styleMainRoute() /*else styleAlternativeRoute()*/
+                    return
+                }
+            }
+        }
+    }
+
+    private fun PolylineMapObject.styleMainRoute() {
+        zIndex = 10f
+        setStrokeColor(ContextCompat.getColor(requireContext(), CommonColors.green))
+        strokeWidth = 5f
+        outlineColor = ContextCompat.getColor(requireContext(), CommonColors.white)
+        outlineWidth = 3f
+    }
+
+    companion object {
+
+        private val START_POSITION = CameraPosition(Point(56.797469, 60.714430), 13.0f, 0f, 0f)
+
+        private val DEFAULT_POINTS = listOf(
+            Point(56.797469, 60.714430),
+            Point(56.815546, 60.605954),
+        )
     }
 }
