0d7cb742f3d52dcc8d549c3f7eb7125b5663011d
sazh_egor <sazhin2003.egor@gmail.com>
2024-04-13 11:09:00 +0500

    add Main Page

---
+++ app/src/main/java/balacods/pp/restorambaapp/fragment/MainPageFragment.kt
@@ -0,0 +1,63 @@
+package balacods.pp.restorambaapp.fragment
+
+import android.content.Context
+import android.content.SharedPreferences
+import android.os.Bundle
+import android.util.Log
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import android.widget.TextView
+import androidx.fragment.app.Fragment
+import balacods.pp.restorambaapp.databinding.FragmentMainBinding
+
+
+class MainPageFragment : Fragment() {
+
+    private lateinit var binding: FragmentMainBinding
+
+    // это будет именем файла настроек
+    private final val APP_PREFERENCES: String = "mysettings"
+    private var APP_PREFERENCES_INSTRUCTIONS: Boolean = false
+
+    var mSettings: SharedPreferences? = null
+
+    override fun onCreateView(
+        inflater: LayoutInflater, container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View {
+        binding = FragmentMainBinding.inflate(inflater, container, false)
+        return binding.root
+    }
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+
+        onClick()
+
+        val str: TextView = binding.idInst
+
+        mSettings = activity?.getSharedPreferences(APP_PREFERENCES, Context.MODE_PRIVATE) ?: return
+
+        if (mSettings!!.contains(APP_PREFERENCES)) {
+            Log.i("contains", mSettings!!.contains(APP_PREFERENCES).toString())
+            APP_PREFERENCES_INSTRUCTIONS = mSettings!!.getBoolean(APP_PREFERENCES, APP_PREFERENCES.toBoolean())
+        }
+
+        if (!APP_PREFERENCES_INSTRUCTIONS) {
+
+            str.visibility = View.VISIBLE
+
+            with (mSettings!!.edit()) {
+                putBoolean(APP_PREFERENCES, true)
+                apply()
+            }
+        }
+    }
+
+    private fun onClick() {
+//        binding.idButtonGetRandomDish.setOnClickListener {
+//            findNavController().navigate(R.id.action_mainFrag_to_yandexCardFrag)
+//        }
+    }
+}
