5e23c445ce36903c3f2a1064d67a7e5c145cae02
sazh_egor <sazhin2003.egor@gmail.com>
2024-04-13 15:39:31 +0500

    fix project: Main Page full

--- app/src/main/java/balacods/pp/restorambaapp/fragment/MainPageFragment.kt
+++ app/src/main/java/balacods/pp/restorambaapp/fragment/MainPageFragment.kt
@@ -1,10 +1,13 @@
 package balacods.pp.restorambaapp.fragment
 
+import android.content.Context
 import android.content.SharedPreferences
 import android.os.Bundle
+import android.util.Log
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import androidx.constraintlayout.widget.ConstraintLayout
 import androidx.fragment.app.Fragment
 import balacods.pp.restorambaapp.databinding.FragmentMainBinding
 
@@ -14,7 +17,7 @@ class MainPageFragment : Fragment() {
     private lateinit var binding: FragmentMainBinding
 
     // это будет именем файла настроек
-    private final val APP_PREFERENCES: String = "mysettings"
+    private val APP_PREFERENCES: String = "instructions"
     private var APP_PREFERENCES_INSTRUCTIONS: Boolean = false
 
     var mSettings: SharedPreferences? = null
@@ -30,33 +33,41 @@ class MainPageFragment : Fragment() {
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
 
+        initInstructions()
+
         onClick()
 
-//        val instructions: ConstraintLayout = binding.idInstructions.root
-//
-//        mSettings = activity?.getSharedPreferences(APP_PREFERENCES, Context.MODE_PRIVATE) ?: return
-//
-//        if (mSettings!!.contains(APP_PREFERENCES)) {
-//            Log.i("contains", mSettings!!.contains(APP_PREFERENCES).toString())
-//            APP_PREFERENCES_INSTRUCTIONS =
-//                mSettings!!.getBoolean(APP_PREFERENCES, APP_PREFERENCES.toBoolean())
-//        }
-//
-//        if (!APP_PREFERENCES_INSTRUCTIONS) {
-//
-//
-//            instructions.visibility = View.VISIBLE
-//
-//            with(mSettings!!.edit()) {
-//                putBoolean(APP_PREFERENCES, true)
-//                apply()
-//            }
-//        }
     }
 
     private fun onClick() {
 //        binding.idButtonGetRandomDish.setOnClickListener {
 //            findNavController().navigate(R.id.action_mainFrag_to_yandexCardFrag)
 //        }
+    }
+
+    private fun initInstructions() {
+        binding.idInstructions.idButtonAgree.setOnClickListener {
+            binding.idInstructions.root.visibility = View.GONE
+        }
+
+        val instructions: ConstraintLayout = binding.idInstructions.root
+
+        mSettings = activity?.getSharedPreferences(APP_PREFERENCES, Context.MODE_PRIVATE) ?: return
+
+        if (mSettings!!.contains(APP_PREFERENCES)) {
+            Log.i("contains", mSettings!!.contains(APP_PREFERENCES).toString())
+            APP_PREFERENCES_INSTRUCTIONS =
+                mSettings!!.getBoolean(APP_PREFERENCES, APP_PREFERENCES.toBoolean())
+        }
+
+        if (!APP_PREFERENCES_INSTRUCTIONS) {
+
+            instructions.visibility = View.VISIBLE
+
+            with(mSettings!!.edit()) {
+                putBoolean(APP_PREFERENCES, true)
+                apply()
+            }
+        }
     }
 }
