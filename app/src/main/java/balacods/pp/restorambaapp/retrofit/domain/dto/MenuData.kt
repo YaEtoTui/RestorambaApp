053f8d9b72c551684907d7f4a7bf4f6b5a5cf01d
Egor Sazhin <sazhin2003.egor@gmail.com>
2024-04-21 16:31:09 +0500

    fix project

---
+++ app/src/main/java/balacods/pp/restorambaapp/retrofit/domain/dto/MenuData.kt
@@ -0,0 +1,20 @@
+package balacods.pp.restorambaapp.retrofit.domain.dto
+
+import com.google.gson.annotations.SerializedName
+
+data class MenuData(
+    @SerializedName("dishes_id")
+    val dishesId: Int,
+    @SerializedName("restaurant_id")
+    val restaurantId: Int,
+    @SerializedName("dish_name")
+    val dishName: String,
+    @SerializedName("dish_description")
+    val dishDescription: String,
+    @SerializedName("dish_price")
+    val dishPrice: String,
+    @SerializedName("dish_weight")
+    val dishWeight: Float,
+    @SerializedName("dish_type")
+    val dishType: String
+)
