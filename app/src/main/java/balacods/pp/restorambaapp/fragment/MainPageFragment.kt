52712e31100d7c7cb77e4d7e21f1661634fe35ae
Egor Sazhin <sazhin2003.egor@gmail.com>
2024-04-21 23:59:34 +0500

    add Switch to page search (beta)

--- app/src/main/java/balacods/pp/restorambaapp/fragment/MainPageFragment.kt
+++ app/src/main/java/balacods/pp/restorambaapp/fragment/MainPageFragment.kt
@@ -47,6 +47,9 @@ class MainPageFragment : Fragment() {
         binding.idNavRestaurants.setOnClickListener {
             findNavController().navigate(R.id.action_mainFrag_to_restaurantsFrag)
         }
+        binding.idNavSearch.setOnClickListener {
+            findNavController().navigate(R.id.action_mainFrag_to_searchFrag)
+        }
 //        binding.idNavMap.setOnClickListener {
 //            findNavController().navigate(R.id.action_mainFrag_to_yandexCardFrag)
 //        }
