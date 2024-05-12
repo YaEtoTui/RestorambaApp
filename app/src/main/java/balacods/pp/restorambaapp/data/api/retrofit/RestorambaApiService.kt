fe9886fb4f74b3279566c6987a2433bec2cb47ab
Egor Sazhin <sazhin2003.egor@gmail.com>
2024-05-12 22:45:24 +0500

    master - bug fixes

--- app/src/main/java/balacods/pp/restorambaapp/data/api/retrofit/RestorambaApiService.kt
+++ app/src/main/java/balacods/pp/restorambaapp/data/api/retrofit/RestorambaApiService.kt
@@ -1,5 +1,11 @@
 package balacods.pp.restorambaapp.data.api.retrofit
 
+import balacods.pp.restorambaapp.data.model.RestaurantData
+import retrofit2.http.GET
+
 interface RestorambaApiService {
+
+    @GET("api/v1/restaurants")
+    suspend fun getListRestaurants() : List<RestaurantData>
 
 }
